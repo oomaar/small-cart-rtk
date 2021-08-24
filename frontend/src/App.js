5aa4a038aa0eaa228d2bf3554bf824c43d0e20c0
Omar <omaar_5@hotmail.com>
2021-08-24 17:55:47 +0200

    setup: Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
-import { Home } from "./screens"
+import { Cart, Home } from "./screens";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/cart" component={Cart} />
+      </Switch>
+    </Router>
   )
 }
