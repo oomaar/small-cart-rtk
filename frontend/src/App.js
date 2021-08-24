e20f1dfb95badac3ef5740710e051fd7f85560ca
Omar <omaar_5@hotmail.com>
2021-08-24 17:58:06 +0200

    setup: Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import { Cart, Home } from "./screens";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar } from "./components";
 
 export const App = () => {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/cart" component={Cart} />
---
+++ frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,8 @@
+export const Navbar = () => {
+    return (
+        <div>
+            <a href="/">Home</a>
+            <a href="/cart">Cart</a>
+        </div>
+    );
+};
