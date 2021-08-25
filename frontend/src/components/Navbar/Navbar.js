e7bc55035ed286b84c5ac6659f740fde82563ff0
Omar <omaar_5@hotmail.com>
2021-08-25 13:48:32 +0200

    part-1: Navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,8 +1,31 @@
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
 export const Navbar = () => {
     return (
-        <div>
-            <a href="/">Home</a>
-            <a href="/cart">Cart</a>
-        </div>
+        <nav className="nav-bar">
+            <Link to="/" className="nav-logo">
+                <h2>Logo</h2>
+            </Link>
+            <Link to="/cart">
+                <div className="nav-logo-container">
+                    <svg
+                        className="nav-svg-icon"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="34"
+                        height="34"
+                        viewBox="0 0 24 24">
+                        <path d="M10 19.5c0 .829-.672 1.5-1.5 1.5s-1.5-.671-1.5-1.5c0-.828.672-1.5 1.5-1.5s1.5.672 1.5 
+                    1.5zm3.5-1.5c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 
+                    1.5-1.5c0-.828-.672-1.5-1.5-1.5zm1.336-5l1.977-7h-16.813l2.938 
+                    7h11.898zm4.969-10l-3.432 12h-12.597l.839 
+                    2h13.239l3.474-12h1.929l.743-2h-4.195z" />
+                    </svg>
+                    <span className="nav-count-container">
+                        <span className="nav-count">0</span>
+                    </span>
+                </div>
+            </Link>
+        </nav>
     );
 };
