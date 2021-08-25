c791d4835daeb0562cc4e222ab82de98764698f0
Omar <omaar_5@hotmail.com>
2021-08-25 16:42:14 +0200

    complete: 404 page

--- frontend/src/screens/NotFound/NotFound.js
+++ frontend/src/screens/NotFound/NotFound.js
@@ -1,7 +1,11 @@
+import "./NotFound.css";
+
 export const NotFound = () => {
     return (
-        <div>
-            404
+        <div className="container notfound-container">
+            <h1 className="notfound-title">404!!</h1>
+            <p className="notfound-text">Ooooooooopssss!</p>
+            <p className="notfound-text">The Page you Requested is not found.</p>
         </div>
     );
 };
