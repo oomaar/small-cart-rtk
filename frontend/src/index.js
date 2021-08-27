78ae4b88e430ce310d5fb59dcfda21912e2326a4
Omar <omaar_5@hotmail.com>
2021-08-27 14:34:48 +0200

    setup: store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,4 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import { App } from './App';
 import "./index.css";
-render(<App />, document.getElementById('root'));
+import { Provider } from "react-redux";
+import { store } from './redux/store';
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root')
+);
