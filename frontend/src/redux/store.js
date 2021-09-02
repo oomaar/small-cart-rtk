dbf9c42ca86bdf39d3982c7175da33847016f23a
Omar <omaar_5@hotmail.com>
2021-09-02 14:11:38 +0200

    setup: cartSlice

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productsReducer, { productsFetch } from "./slices/productsSlice";
+import cartSliceReducer from "./slices/cartSlice";
 
 export const store = configureStore({
     reducer: {
-        products: productsReducer
+        products: productsReducer,
+        cart: cartSliceReducer
     }
 });
 
