32f7356c476f5214c4278cd2d17551872082239c
Omar <omaar_5@hotmail.com>
2021-09-02 23:21:02 +0200

    feat: toastify state & save state to localstorage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import { Cart, Home, NotFound } from "./screens";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import { Navbar } from "./components";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const App = () => {
   return (
     <Router>
+      <ToastContainer />
       <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
