88fc3eada626bec76ab363fe71a1778d3f7a61b0
Omar <omaar_5@hotmail.com>
2021-09-02 14:27:00 +0200

    dispatch: addToCart

--- frontend/src/screens/Home/Home.js
+++ frontend/src/screens/Home/Home.js
@@ -1,9 +1,12 @@
 import { selectItems } from "../../redux/slices/productsSlice";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import "./Home.css";
+import { addToCart } from "../../redux/slices/cartSlice";
 
 export const Home = () => {
     const items = useSelector(selectItems);
+    const dispatch = useDispatch();
+    const handleAddToCart = product => dispatch(addToCart(product));
 
     const products = items.map(product => (
         <div key={product.id} className="product__container">
@@ -17,7 +20,7 @@ export const Home = () => {
                 <p className="product__description">{product.description}</p>
                 <span className="product__price">${product.price}</span>
             </div>
-            <button className="product__button">Add to Cart</button>
+            <button className="product__button" onClick={() => handleAddToCart(product)}>Add to Cart</button>
         </div>
     ));
 
