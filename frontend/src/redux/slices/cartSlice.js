6717048a3729266cb25d170d34567bce8552bffc
Omar <omaar_5@hotmail.com>
2021-09-03 02:14:02 +0200

    feat setup decreaseCart logic in cartSlice.js

--- frontend/src/redux/slices/cartSlice.js
+++ frontend/src/redux/slices/cartSlice.js
@@ -50,6 +50,41 @@ const cartSlice = createSlice({
             localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
             return state;
         },
+        decreaseCart: (state, action) => {
+            state.cartItems.map((cartItem) => {
+                if (cartItem.id === action.payload.id) {
+                    if (cartItem.cartQuantity > 1) {
+                        cartItem = {
+                            ...cartItem,
+                            cartQuantity: cartItem.cartQuantity - 1,
+                        };
+
+                        const index = state.cartItems.findIndex(cartItem => cartItem.id === action.payload.id);
+                        state.cartItems[index] = cartItem;
+
+                        toast.info(`Decreased ${action.payload.name} quantity`, {
+                            position: "bottom-left",
+                        });
+                    } else if (cartItem.cartQuantity === 1) {
+                        const index = state.cartItems.findIndex(cartItem => cartItem.id === action.payload.id);
+                        let newCart = [...state.cartItems];
+
+                        if (index <= 0) {
+                            newCart.splice(index, 1);
+                            state.cartItems = newCart;
+                            toast.error(`${action.payload.name} Removed from Cart`, { position: "bottom-left" });
+                        } else {
+                            console.warn(
+                                `Cant remove product (id: ${action.payload.id}) as it's not in the cart`
+                            );
+                        };
+                    };
+                };
+
+                localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+                return state;
+            });
+        },
     },
 });
 
@@ -57,5 +92,5 @@ export const selectItems = state => state.cart.cartItems;
 export const selectQuantity = state => state.cart.cartTotalQuantity;
 export const selectTotalAmount = state => state.cart.cartTotalAmount;
 
-export const { addToCart, removeFromCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, decreaseCart } = cartSlice.actions;
 export default cartSlice.reducer;
